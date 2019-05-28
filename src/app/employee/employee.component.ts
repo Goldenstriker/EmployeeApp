f1177e0630ce8ba625770d135c43b7d7d6737c1d
Goldenstriker <ndparab240@gmail.com>
2019-05-28 12:58:49 +0530

    2 files modified

--- src/app/employee/employee.component.ts
+++ src/app/employee/employee.component.ts
@@ -7,7 +7,7 @@ import {Employee} from '../employee';
 })
 export class EmployeeComponent implements OnInit {
 
-  employee:Employee[]=[{id:101,name:'Ninad'},{id:102,name:'Prajakta'},{id:103,name:'SK'}];
+  employee:Employee[]=[{id:101,name:'Ninad'},{id:102,name:'Prajakta'},{id:103,name:'SK'},{id:104,name:'TV'}];
   delete(emp:Employee):void{
   this.employee.splice(this.employee.indexOf(emp),1);
   }
