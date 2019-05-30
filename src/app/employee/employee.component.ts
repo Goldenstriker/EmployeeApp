53c40dde531d8d46e727c03d355212b82f5a66ec
Goldenstriker <ndparab240@gmail.com>
2019-05-30 14:26:37 +0530

    1 file modified

--- src/app/employee/employee.component.ts
+++ src/app/employee/employee.component.ts
@@ -11,8 +11,8 @@ export class EmployeeComponent implements OnInit {
 
   employees: Employee[];
   delete(emp: Employee): void {
-    this.employees.splice(this.employees.indexOf(emp), 1);
-    this.employeeService.delete_employee(emp.id).subscribe((res)=>this.getEmployees());
+    //this.employees.splice(this.employees.indexOf(emp), 1);
+    this.employeeService.delete_employee(emp.id).subscribe();
   }
   constructor(public employeeService: EmployeeService) { }
   ngOnInit() {
