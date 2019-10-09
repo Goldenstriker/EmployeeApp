57e0d18e2e4acd27af30e2b62db536054b3bcc25
Goldenstriker <ndparab240@gmail.com>
2019-10-09 19:24:14 +0530

    1 file modified

--- src/app/post.service.ts
+++ src/app/post.service.ts
@@ -3,10 +3,13 @@ import {HttpClient} from '@angular/common/http';
 import {Post} from  './post'; 
 @Injectable()
 export class PostService {
-  baseUrl:string="https://jsonplaceholder.typicode.com";
+  //baseUrl:string="https://jsonplaceholder.typicode.com";
+  //https://dataprovider.goldenstriker.repl.co/GetData/
+  baseUrl:string="https://dataprovider.goldenstriker.repl.co/GetData/";
   constructor(private httpClient:HttpClient) { }
   get_posts(){
-    return this.httpClient.get<Post[]>(this.baseUrl+'/posts');
+    //return this.httpClient.get<Post[]>(this.baseUrl+'/posts');
+    return this.httpClient.get<Post[]>(this.baseUrl);
   }
 
 }
